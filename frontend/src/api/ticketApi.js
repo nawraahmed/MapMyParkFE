b423c3dec32d0616fb111222e2fc90e6b3ddebe2
NoorAlJirdabi <noor.aljirdabi@gmail.com>
2024-10-16 08:56:02 +0300

    complete ticket

--- frontend/src/api/ticketApi.js
+++ frontend/src/api/ticketApi.js
@@ -2,11 +2,19 @@ import axios from 'axios'
 
 const API_URL = 'http://localhost:4000/api/tickets'
 
+const getAuthHeader = () => {
+  const token = localStorage.getItem('token') // Get the token from local storage
+  return { Authorization: `Bearer ${token}` }
+}
+
 const ticketApi = {
-  createTicket: (ticketData) => axios.post(API_URL, ticketData),
-  getTickets: () => axios.get(API_URL),
-  getTicket: (id) => axios.get(`${API_URL}/${id}`),
-  deleteTicket: (id) => axios.delete(`${API_URL}/${id}`)
+  createTicket: (ticketData) =>
+    axios.post(API_URL, ticketData, { headers: getAuthHeader() }),
+  getTickets: () => axios.get(API_URL, { headers: getAuthHeader() }),
+  getTicket: (id) =>
+    axios.get(`${API_URL}/${id}`, { headers: getAuthHeader() }),
+  deleteTicket: (id) =>
+    axios.delete(`${API_URL}/${id}`, { headers: getAuthHeader() })
 }
 
 export default ticketApi
